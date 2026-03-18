a31a7f4c68b6c024b24ae55ca247cd833eeb5d74
yanis <yanis.dolivet@epitech.eu>
2026-03-18 15:06:06 +0100

    add: route for creating publication in the db

--- src/modules/publication/controllers/publication.controller.ts
+++ src/modules/publication/controllers/publication.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Request,
+} from '@nestjs/common';
+import { Public } from '@/decorators/public.decorator';
+import { PublicationService } from '../services/publication.service';
 
 @Controller('publication')
-export class PublicationController {}
+export class PublicationController {
+    constructor(private publicationService: PublicationService) {}
+
+  @Public()
+  @HttpCode(HttpStatus.OK)
+  @Post('create')
+  create(@Body() createDto: Record<string, any>) {
+    return this.publicationService.create(createDto);
+  }
+}
--- src/modules/publication/services/publication.service.ts
+++ src/modules/publication/services/publication.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { PostPublicationDto, Publication } from '../models/publication.model';
+import { start } from 'repl';
 
 @Injectable()
-export class PublicationService {}
+export class PublicationService {
+    constructor(@InjectModel(Publication) private publicationModel: typeof Publication) {}
+
+    async create(publication: Partial<Publication>) {
+        const nextId =
+            publication.id ??
+            (((await this.publicationModel.max('id')) as number | null) ?? 0) + 1;
+
+        return this.publicationModel.create({
+            ...publication,
+            id: nextId,
+            create_at: publication.createdAt ?? new Date(),
+            starting_date: publication.starting_date ?? new Date(),
+            ending_date: publication.ending_date ?? new Date(),
+        });
+    }
+}
