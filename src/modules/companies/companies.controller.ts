804036b7ed821b355bf6575a104f11f8b47daac1
yanismgt <yanis.mignot@epitech.eu>
2026-04-09 22:15:48 +0200

    fix: useguard deleted

--- src/modules/companies/companies.controller.ts
+++ src/modules/companies/companies.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Request, UseGuards } from '@nestjs/common';
+import { Controller, Get, Request } from '@nestjs/common';
 import type { Request as ExpressRequest } from 'express';
 import { CompaniesService } from './services/companies.service';
 
