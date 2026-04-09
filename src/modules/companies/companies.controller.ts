8e52269b0534e8b5cb833bf8fb6662200f522632
yanismgt <yanis.mignot@epitech.eu>
2026-04-09 22:13:56 +0200

    fix: authguard deleted

--- src/modules/companies/companies.controller.ts
+++ src/modules/companies/companies.controller.ts
@@ -1,7 +1,6 @@
 import { Controller, Get, Request, UseGuards } from '@nestjs/common';
 import type { Request as ExpressRequest } from 'express';
 import { CompaniesService } from './services/companies.service';
-import { AuthGuard } from '@/modules/auth/auth.guard';
 
 interface RequestWithUser extends ExpressRequest {
   user: {
@@ -14,7 +13,6 @@ interface RequestWithUser extends ExpressRequest {
 export class CompaniesController {
   constructor(private readonly companiesService: CompaniesService) {}
 
-  @UseGuards(AuthGuard)
   @Get('my-companies')
   async getMyCompanies(@Request() req: RequestWithUser) {
     const userId = req.user.sub;
