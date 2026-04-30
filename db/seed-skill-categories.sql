2a711e3f3a7da95092dd7be1ebf56b09065be804
yanisdolivet <yanis.dolivet@epitech.eu>
2026-04-30 14:35:47 +0200

    fix: correct value into the script for the skill category

--- db/seed-skill-categories.sql
+++ db/seed-skill-categories.sql
@@ -1,16 +1,7 @@
 -- Seed data for skill_category table
 
 INSERT INTO "skill_category" ("name") VALUES
-  ('IT & Development'),
-  ('Design & Creative'),
-  ('Sales & Marketing'),
-  ('Writing & Translation'),
-  ('Admin & Customer Support'),
-  ('Finance & Accounting'),
-  ('Engineering & Architecture'),
-  ('Legal'),
-  ('HR & Recruiting'),
-  ('Manual Labor & Trades'),
-  ('Transportation & Logistics'),
-  ('Healthcare & Wellness')
+  ('Restaurant FOH'),
+  ('Restaurant BOH'),
+  ('Coffee'),
 ON CONFLICT ("name") DO NOTHING;
