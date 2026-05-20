b5cb74d0f19a21f00e624079061582e2ae4a2064
yanismgt <yanis.mignot@epitech.eu>
2026-05-20 11:49:16 +0200

    feat: adress handling

--- src/modules/address/address.service.ts
+++ src/modules/address/address.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 import { Address } from './address.model';
-import { CreateAddressDto } from './address.dto';
+import { CreateAddressDto, GetMapAddressesDto } from './address.dto';
+import { Sequelize } from 'sequelize-typescript';
 
 @Injectable()
 export class AddressService {
-  constructor(@InjectModel(Address) private readonly addressModel: typeof Address) {}
+  constructor(
+    @InjectModel(Address) private readonly addressModel: typeof Address,
+    private readonly sequelize: Sequelize,
+  ) {}
 
   async createAddress(userId: number, body: CreateAddressDto) {
     const completeAddress = `${body.street_number} ${body.street_name}, ${body.zip_code} ${body.city}, ${body.country}`;
@@ -16,5 +20,21 @@ export class AddressService {
     });
 
     return address;
+  }
+
+  async getAddressesInZone(dto: GetMapAddressesDto) {
+    const { min_lat, max_lat, min_lng, max_lng } = dto;
+
+    const query = `
+      SELECT id, latitude, longitude
+      FROM "address"
+      WHERE geom && ST_MakeEnvelope(:min_lng, :min_lat, :max_lng, :max_lat, 4326)
+      LIMIT 300
+    `;
+
+    return this.sequelize.query(query, {
+      replacements: { min_lat, max_lat, min_lng, max_lng },
+      type: 'SELECT',
+    });
   }
 }
