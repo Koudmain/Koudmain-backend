e52f4c87c9fc4130a8382b7d3ad86dc8d67d9f7b
yanisdolivet <yanis.dolivet@epitech.eu>
2026-05-29 17:34:23 +0200

    fix: adding the skill dependency to publication

--- src/modules/publication/publication.module.ts
+++ src/modules/publication/publication.module.ts
@@ -4,11 +4,12 @@ import { PublicationController } from './controllers/publication.controller';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { Publication } from './models/publication.model';
 import { PublicationSkill } from './models/publication-skill.model';
+import { Skill } from '@/modules/skill/models/skill.model';
 
 @Module({
   providers: [PublicationService],
   controllers: [PublicationController],
-  imports: [SequelizeModule.forFeature([Publication, PublicationSkill])],
+  imports: [SequelizeModule.forFeature([Publication, PublicationSkill, Skill])],
   exports: [PublicationService],
 })
 export class PublicationModule {}
