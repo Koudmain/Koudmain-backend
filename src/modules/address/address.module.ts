7c3ff20148362274295fd81764d07beaba7a333e
yanismgt <yanis.mignot@epitech.eu>
2026-06-03 13:46:42 +0200

    fix: provider fix

--- src/modules/address/address.module.ts
+++ src/modules/address/address.module.ts
@@ -4,11 +4,12 @@ import { AddressService } from './services/address.service';
 import { AuthModule } from '@/modules/auth/auth.module';
 import { Address } from './address.model';
 import { SequelizeModule } from '@nestjs/sequelize';
+import { GeocodingService } from '@/common/utils/geocoding/geocoding.service';
 
 @Module({
   imports: [SequelizeModule.forFeature([Address]), forwardRef(() => AuthModule)],
   controllers: [AddressController],
-  providers: [AddressService],
+  providers: [AddressService, GeocodingService],
   exports: [AddressService],
 })
 export class AddressModule {}
