0826c6af4080794474dab3d657c8b91a136c2488
Thomas Roche-Rebelle <thomas.roche-rebelle@epitech.eu>
2026-07-12 16:54:52 +0200

    fix: add correct values for unit tests requirements

--- test/jest.unit.js
+++ test/jest.unit.js
@@ -34,10 +34,10 @@ module.exports = {
   ],
   coverageThreshold: {
     global: {
-      branches: 50,
-      functions: 50,
-      lines: 50,
-      statements: 50,
+      branches: 60,
+      functions: 60,
+      lines: 80,
+      statements: 60,
     },
   },
 };
