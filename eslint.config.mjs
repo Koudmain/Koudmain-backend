306a4393ce3af28dbd74f0c4611d422aa73aac46
Thomas Roche-Rebelle <thomas.roche-rebelle@epitech.eu>
2026-07-18 19:48:51 +0200

    fix: Fix of simple pre-commit errors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,7 +7,7 @@ import unicorn from 'eslint-plugin-unicorn';
 
 export default tseslint.config(
   {
-    ignores: ['eslint.config.mjs', 'dist/**', 'node_modules/**', 'coverage/**', 'migrations/**', 'seeders/**', 'test/jest.*.js'],
+    ignores: ['eslint.config.mjs', 'dist/**', 'node_modules/**', 'coverage/**', 'migrations/**', 'seeders/**', 'test/**'],
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
